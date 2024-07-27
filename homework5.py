497b892ddd2280db50abdf69ef170d354bed7726
DmitriyNal <dima7kiper@yandex.ru>
2024-07-27 22:48:36 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,10 @@
+immutable_var = 2024,'GOAL',True
+print(immutable_var)
+#immutable_var[0]=0
+#print(immutable_var)
+
+mutable_list=[5,7,'numder',True]
+mutable_list[2]='month'
+print(mutable_list)
+
+
